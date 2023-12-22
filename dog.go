65dd589c2e90407388e9aeb7d16fbe28cf744eeb
Hussnain Jutt <huxxnainali@gmail.com>
2023-12-22 16:33:25 +0000

    first commit

---
+++ dog.go
@@ -0,0 +1,10 @@
+package dog
+
+import("strings")
+
+func WhenGrownUp(s string) string{
+
+  return "When the puppy grows up it says" + strings.ToUpper(s)
+
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/huxxnainjutt/dog
+
+go 1.21.5
